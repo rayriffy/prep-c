c2eb846c6d15a13e0e62888351247af08f0868eb
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-07-05 11:12:44 +0700

    ✨ Oiginal copy

---
+++ ex3-7/2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int DATA[8] = {0, 1, 2, 6, 7, 5, 4, 3};
+int i = 0;
+int max_y = 0;
+
+int times() {
+  return DATA[i] * DATA[i+1];
+}
+
+void loop(int len) {
+  int y;
+  len = len - 1;
+  for (int i=0; i<len; i++) {
+    y = times();
+    if (max_y < y) {
+      max_y = y;
+    }
+  }
+}
+
+int main(){
+  loop(8);
+  printf("The answer is %d", max_y);
+  return 0;
+}
