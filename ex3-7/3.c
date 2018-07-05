cf7643dc6526ac9258b9ef267d19d63545afcc41
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-07-05 11:29:32 +0700

    🐛 Minor fix

--- ex3-7/3.c
+++ ex3-7/3.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
+float g=9.81;
+
+float dis(float t) {
+  return g*pow(t,2)/2;
+}
+
+float vel(float t) {
+  return g*t;
+}
+
 int main(int argc, char const *argv[])
 {
-  float a,t;
-  scanf("%f %f",&a,&t);
-  printf("%f %f",a*pow(t,2)/2,a*t);
+  float t;
+  scanf("%f",&t);
+  printf("%f %f",dis(t),vel(t));
   return 0;
 }
